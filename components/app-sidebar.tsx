b28c2b7997c72f103599e2c4b6919be8a70d4a6e
TypTech <eglsederthomas1@gmail.com>
2025-07-31 20:11:24 +0200

    Update sidebar version display to Pre-Release v0.0.3

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -135,7 +135,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </div>
                 <div className="flex flex-col gap-0.5 leading-none">
                   <span className="font-semibold text-base">ServerDash</span>
-                  <span className="text-xs text-sidebar-foreground/70">Pre-Release v0.0.2</span>
+                  <span className="text-xs text-sidebar-foreground/70">Pre-Release v0.0.3</span>
                 </div>
               </a>
             </SidebarMenuButton>
